98a05768ccab92b072bd8d8f4a75e464278d876a
kene <97908971+Kene33@users.noreply.github.com>
2024-06-18 22:32:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,28 @@ import cv2
 from pyzbar import pyzbar
 
 
-def makeqr():
+def make_qr():
+    link = str(input("Write link or any text for make QR Code > "))
+    qrcode = segno.make(link, micro=False)
+    qrcode.save("qrcode.png", scale=6)
+
+    print("Done! I save your QR Code in qrcode.png")
+
+    open_qr = str(input("Open the QR code or exit? (open/exit) > "))
+    if open_qr.lower() == "open":
+        qrcode.show()
+        print("Bye Bye!")
+        break
+    elif open_qr.lower() == "exit":
+        print("Bye Bye!")
+        break
+    else:
+        print("WRITE ONLY open OR exit :)")
+        make_qr()
+
+def decode_qr():
     while True:
-        link = str(input("Write link or any text for make QR Code > "))
-        qrcode = segno.make(link, micro=False)
-        qrcode.save("qrcode.png", scale=6)
-
-        print("Done! I save your QR Code in qrcode.png")
-
-        open_qr = str(input("open QR or exit? > "))
-        if open_qr == "open":
-            qrcode.show()
-            print("Bye Bye!")
-            break
-        elif open_qr == "exit":
-            print("Bye Bye!")
-            break
-        else:
-            print("WRITE ONLY open OR exit :)")
-            makeqr()
-
-def openqr():
-    while True:
-        qrlink = str(input("Link to QR Code file > "))
+        qrlink = str(input("Enter the path to the QR Code image > "))
         img = cv2.imread(qrlink)
         barcodes = pyzbar.decode(img)
 
@@ -33,27 +32,27 @@ def openqr():
             qrdata = barcode.data.decode("utf-8")
             print(f"Result: {qrdata}")
         
-        print("You want decode more QR? (Yes or No)")
+        print("Do you want to decode more QR codes? (Yes/No) > ")
         quas = str(input(">> "))
-        if quas == "Yes":
-            decodeqr()
+        if quas.lower() == "yes":
+            decode_qr()
         else:
             break
 
 
 def start():
-    print("""What do you want?
-    1) Make QR Code
-    2) Decode QR code""")
-    change = str(input(">> "))
-
-    if change == "1":
-        makeqr()
-    elif change == "2":
-        openqr()
-    else:
-        print("No, only 1 or 2")
-        start()
-
-print("Hi, i can make QR code with your text or decode your QR code.")
+    print("Hi, I can make a QR code with your text or decode your QR code.")
+    while True:
+        choice = input("What do you want to do? (1 - Make QR Code, 2 - Decode QR code, exit - Quit) > ")
+        if choice == "1":
+            make_qr()
+            break
+        elif choice == "2":
+            decode_qr()
+            break
+        elif choice.lower() == "exit":
+            print("Goodbye!")
+            break
+        else:
+            print("Please enter only '1', '2', or 'exit'")
 start()
